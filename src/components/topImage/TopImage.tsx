76f237a543f1d6f516a1323893e6e3154c572ceb
doYoungAn <jispoop@gmail.com>
2024-09-11 14:36:37 +0900

    물결 애니메이션 추가

--- src/components/topImage/TopImage.tsx
+++ src/components/topImage/TopImage.tsx
@@ -1,10 +1,54 @@
 import { FC } from 'react';
+import Wave from 'react-wavify'
 import TopImg from './../../assets/optimization/na_u_us/us_9.webp';
 
 const TopImage: FC = () => {
 
   return (
-    <img src={TopImg} />
+    <div className=' relative'>
+      <div className='absolute top-0 rotate-180'>
+        <Wave fill="url(#gradient)" style={{ display: 'flex', width: '100vw', height: 60 }}>
+          <defs>
+            <linearGradient id="gradient" gradientTransform="rotate(90)">
+              <stop offset="70%"  stopColor="rgba(255,255,255, 0.6)" />
+              <stop offset="30%" stopColor="rgba(255,255,255, 0.6)" />
+            </linearGradient>
+          </defs>
+        </Wave>
+      </div>
+      <div className=' absolute top-0 rotate-180'>
+        <Wave fill="url(#gradient)" style={{ display: 'flex', width: '100vw', height: 60 }}>
+          <defs>
+            <linearGradient id="gradient" gradientTransform="rotate(90)">
+              <stop offset="70%"  stopColor="rgba(255,255,255, 0.6)" />
+              <stop offset="30%" stopColor="rgba(255,255,255, 0.6)" />
+            </linearGradient>
+          </defs>
+        </Wave>
+      </div>
+      <img src={TopImg} alt='' />
+      
+      <div className='absolute bottom-0'>
+        <Wave fill="url(#gradient)" style={{ display: 'flex', width: '100vw', height: 60 }}>
+          <defs>
+            <linearGradient id="gradient" gradientTransform="rotate(90)">
+              <stop offset="70%"  stopColor="rgba(255,255,255, 0.6)" />
+              <stop offset="30%" stopColor="rgba(255,255,255, 0.6)" />
+            </linearGradient>
+          </defs>
+        </Wave>
+      </div>
+      <div className=' absolute bottom-0'>
+        <Wave fill="url(#gradient)" style={{ display: 'flex', width: '100vw', height: 60 }}>
+          <defs>
+            <linearGradient id="gradient" gradientTransform="rotate(90)">
+              <stop offset="70%"  stopColor="rgba(255,255,255, 0.6)" />
+              <stop offset="30%" stopColor="rgba(255,255,255, 0.6)" />
+            </linearGradient>
+          </defs>
+        </Wave>
+      </div>
+    </div>
   )
 }
 
