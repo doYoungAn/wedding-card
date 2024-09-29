6c82bb78f4f4fd7853982873fe4b54aa605c1495
doYoungAn <jispoop@gmail.com>
2024-09-29 16:37:18 +0900

    eslint 및 prettier 적용

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
