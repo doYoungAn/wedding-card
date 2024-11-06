0d9adf7ea4b8338bfd5dccb20924a52608b731cd
doYoungAn <jispoop@gmail.com>
2024-11-06 10:31:36 +0900

    이미지 최적화

--- script/jpgToWebp.js
+++ script/jpgToWebp.js
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+
 const sharp = require('sharp');
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 
-const targetNaUUsDir = path.join(__dirname, './../src/assets/optimization/na_u_us');
+const targetNaUUsDir = path.join(
+  __dirname,
+  './../src/assets/optimization/na_u_us'
+);
 const folderNaUUsPath = path.join(__dirname, './../src/assets/na_u_us');
 const naUUsfileNames = fs.readdirSync(folderNaUUsPath);
 
@@ -13,21 +19,25 @@ const wSfileNames = fs.readdirSync(folderWsPath);
 const naUUsfiles = naUUsfileNames.map((fileName) => {
   return {
     fileName,
-    buffer: fs.readFileSync(`${folderNaUUsPath}/${fileName}`)
-  }
-})
+    buffer: fs.readFileSync(`${folderNaUUsPath}/${fileName}`),
+  };
+});
 
 naUUsfiles.forEach((file) => {
-  sharp(file.buffer).webp({ quality: 100 }).toFile(`${targetNaUUsDir}/${file.fileName.split('.')[0]}.webp`)
-})
+  sharp(file.buffer)
+    .webp({ quality: 90 })
+    .toFile(`${targetNaUUsDir}/${file.fileName.split('.')[0]}.webp`);
+});
 
 const wSfiles = wSfileNames.map((fileName) => {
   return {
     fileName,
-    buffer: fs.readFileSync(`${folderWsPath}/${fileName}`)
-  }
-})
+    buffer: fs.readFileSync(`${folderWsPath}/${fileName}`),
+  };
+});
 
 wSfiles.forEach((file) => {
-  sharp(file.buffer).webp({ quality: 100 }).toFile(`${targetWsDir}/${file.fileName.split('.')[0]}.webp`)
-})
+  sharp(file.buffer)
+    .webp({ quality: 90 })
+    .toFile(`${targetWsDir}/${file.fileName.split('.')[0]}.webp`);
+});
